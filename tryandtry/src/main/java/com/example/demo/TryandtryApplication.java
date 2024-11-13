38ad98b4b032879cb22a510434a1cf35a26252c7
Annopqr <science0813@naver.com>
2024-11-13 18:01:17 +0900

    프로젝트 생성

---
+++ tryandtry/src/main/java/com/example/demo/TryandtryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryandtryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryandtryApplication.class, args);
+	}
+
+}
